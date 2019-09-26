d98270931fd0972580b696405b671de766d6b4e9
mcnichol <mcnichol.m@gmail.com>
2019-09-26 17:41:00 -0500

    Styling edits, updates to formatting approach, verbiage cleanp

--- imperative-commands.sh
+++ imperative-commands.sh
@@ -1,58 +1,58 @@
+FG_CYAN="\e[36m"
+FG_BLUE="\e[34m"
+FG_RED="\e[31m"
+FG_LT_GREEN="\e[92m"
+
+RESET_ALL="\e[0m"
 bold () {
-    echo "\e[1m$1\e[0m" 
+    echo "\e[1m$1$RESET_ALL" 
 }
 
-cyan () {
-    echo "\e[36m$1\e[0m" 
-}
-
-blue () {
-    echo "\e[34m$1\e[0m" 
-}
-
-red () {
-    echo "\e[31m$1\e[0m" 
+colorize () {
+    echo "$1$2$RESET_ALL" 
 }
 
 TIPS=()
 
 TIPS+=("
-$(cyan "--dry-run")
+\n\n\t$(colorize "$FG_CYAN" "--dry-run")
 
-The $(cyan "--dry-run") option will $(bold "not create the resource"). It will tell you if the $(bold "command is correct") and the resource can be created.
+\t$(bold "Explanation:")
+\tThe $(colorize "$FG_CYAN" "--dry-run") option will $(bold "not create the resource"). It will tell you if the $(bold "command is correct") and the resource can be created.
 ")
 
-TIPS+=( "-o yaml | json | wide | name | custom-columns{,-file} | go-template{,-file} | jsonpath{,-file}
+TIPS+=("
+\n\n\t$(colorize "$FG_CYAN" "-o yaml | json | wide | name | custom-columns{,-file} | go-template{,-file} | jsonpath{,-file}")
 
-Output the resource in a supported format of your choice. 
+\tOutput the resource in a supported format of your choice. 
 
-For more documentation on templates/files see below:
-    
-[custom-columns](http://kubernetes.io/docs/user-guide/kubectl-overview/#custom-columns)
-[go-template](http://golang.org/pkg/text/template/#pkg-overview)
-[jsonpath](http://kubernetes.io/docs/user-guide/jsonpath)
+\tFor more documentation on templates/files see below:
+\t    custom-columns - ($(colorize "$FG_LT_GREEN" "http://kubernetes.io/docs/user-guide/kubectl-overview/#custom-columns"))
+\t    go-template]   - ($(colorize "$FG_LT_GREEN" "http://golang.org/pkg/text/template/#pkg-overview"))
+\t    jsonpath       - ($(colorize "$FG_LT_GREEN" "http://kubernetes.io/docs/user-guide/jsonpath"))
 ")
 
-TIPS+=( $'
-kubectl run --generator=run-pod/v1 nginx --image=nginx --dry-run -o yaml > pod-definition.yaml
+TIPS+=(" 
+\n\n\t$(colorize "$FG_CYAN" "kubectl run --generator=run-pod/v1 nginx --image=nginx --dry-run -o yaml > pod-definition.yaml")
 
-Use the flags --dry-run and -o yaml in combination with a generator to output a resource definition file quickly. 
-' )
+\tUse the flags $(colorize "$FG_CYAN" "--dry-run") and $(colorize "$FG_CYAN" "-o yaml") in combination with a $(colorize "$FG_CYAN" "--generator") to output a resource definition file quickly. 
+")
 
 
-TIPS+=( $'
-kubectl run --generator=run-pod/v1 nginx --image=nginx
+TIPS+=(" 
+\n\n\t$(colorize "$FG_CYAN" "kubectl run --generator=run-pod/v1 nginx --image=nginx")
 
-Easily create an nginx pod using a generator
-' )
 
-TIPS+=( $'
-kubectl run --generator=run-pod/v1 nginx --image=nginx --dry-run -o yaml
+\tCreate an nginx pod using a generator
+")
 
-Generate POD Manifest YAML files (-o yaml) and don\'t create it(--dry-run)
-' )
+TIPS+=(" 
+\n\n\t$(colorize "$FG_CYAN" "kubectl run --generator=run-pod/v1 nginx --image=nginx --dry-run -o yaml")
 
-TIPS+=( $'
+\tCreate an nginx pod using a generator but do not create it ($(colorize "$FG_CYAN" "--dry-run")) and output as yaml ($(colorize "$FG_CYAN" "-o yaml"))
+")
+
+TIPS+=("
 Recommended:
 kubectl create deployment --image=nginx nginx
 
@@ -61,9 +61,9 @@ kubectl run --generator=deployment/v1beta1 nginx --image=nginx
 
 Explanation:
 Generate a deployment using nginx image
-' )
+")
 
-TIPS+=( $'
+TIPS+=("
 Generate a deployment file without creating it and output in yaml
 
 Deprecated:
@@ -71,45 +71,47 @@ kubectl run --generator=deployment/v1beta1 nginx --image=nginx --dry-run -o yaml
 
 Recommended:
 kubectl create deployment --image=nginx nginx --dry-run -o yaml
-' )
+")
 
-TIPS+=( $'
+TIPS+=(" 
+$(colorize "$FG_CYAN" "kubectl run --generator=deployment/v1beta1 nginx --image=nginx --replicas=4 --dry-run -o yaml")
+
 Generate a deployment with 4 replicas without creating it and output in yaml
 
-Deprecated:
-kubectl run --generator=deployment/v1beta1 nginx --image=nginx --replicas=4 --dry-run -o yaml
-
 Note:
-kubectl create deployment does not have a --replicas option. You could first create it and then scale it using the kubectl scale command. 
-' )
+kubectl create deployment does not have a --replicas option. You could alternatively create it and kubectl scale. 
+")
 
-#Service
-#
-#Create a Service named redis-service of type ClusterIP to expose pod redis on port 6379
-#
-#kubectl expose pod redis --port=6379 --name redis-service --dry-run -o yaml
-#
-#(This will automatically use the pod's labels as selectors)
-#
-#Or
-#
-#kubectl create service clusterip redis --tcp=6379:6379 --dry-run -o yaml  (This will not use the pods labels as selectors, instead it will assume selectors as app=redis. You cannot pass in selectors as an option. So it does not work very well if your pod has a different label set. So generate the file and modify the selectors before creating the service)
-#
-#
-#Create a Service named nginx of type NodePort to expose pod nginx's port 80 on port 30080 on the nodes:
-#
-#kubectl expose pod nginx --port=80 --name nginx-service --dry-run -o yaml
-#
-#(This will automatically use the pod's labels as selectors, but you cannot specify the node port. You have to generate a definition file and then add the node port in manually before creating the service with the pod.)
-#
-#Or
-#
-#kubectl create service nodeport nginx --tcp=80:80 --node-port=30080 --dry-run -o yaml
-#
-#(This will not use the pods labels as selectors)
-#
-#Both the above commands have their own challenges. While one of it cannot accept a selector the other cannot accept a node port. I would recommend going with the kubectl expose command. If you need to specify a node port, generate a definition file using the same command and manually input the nodeport before creating the service.
-#"""
+TIPS+=("
+$(colorize "$FG_CYAN" "kubectl expose pod redis --port=6379 --name redis-service --dry-run -o yaml")
+
+Create a Service named redis-service of type ClusterIP to expose pod redis on port 6379. 
+
+This will automatically use the pod's labels as selectors
+")
+
+TIPS+=("
+$(colorize "$FG_CYAN" "kubectl create service clusterip redis --tcp=6379:6379 --dry-run -o yaml")
+
+This will not use the pods labels as selectors, instead it will assume selectors as app=redis. You cannot pass in selectors as an option. So it does not work very well if your pod has a different label set. So generate the file and modify the selectors before creating the service
+")
+
+TIPS+=("
+$(colorize "$FG_CYAN" "kubectl expose pod nginx --port=80 --name nginx-service --dry-run -o yaml")
+
+Create a Service named nginx of type NodePort to expose pod nginx's port 80 on port 30080 on the nodes:
+
+This will automatically use the pod's labels as selectors, but you cannot specify the node port. You have to generate a definition file and then add the node port in manually before creating the service with the pod.
+
+")
+
+TIPS+=("
+$(colorize "$FG_CYAN" "kubectl create service nodeport nginx --tcp=80:80 --node-port=30080 --dry-run -o yaml")
+
+(This will not use the pods labels as selectors)
+
+Both the above commands have their own challenges. While one of it cannot accept a selector the other cannot accept a node port. I would recommend going with the kubectl expose command. If you need to specify a node port, generate a definition file using the same command and manually input the nodeport before creating the service.
+")
 
 for tip in "${TIPS[@]}"; do
     clear
