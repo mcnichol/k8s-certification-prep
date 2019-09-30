2f98c158f9585ccdcb30b61097945bc0daba1dd0
Michael McNichol <mcnichol.m@gmail.com>
2019-09-30 12:04:53 -0400

    Check if  user  created  namespace and alert  success

--- configure-pods-and-containers/01_assign-memory-resources-to-containers-and-pods.sh
+++ configure-pods-and-containers/01_assign-memory-resources-to-containers-and-pods.sh
@@ -1,4 +1,15 @@
-# Before you begin you need to have K8s cluster and the kubectl command-line tool congfigured to communicate with your cluster. 
+KUBECTL_CMD=""
+
+if [ "$(uname)" == "Darwin" ]; then
+  echo "Using Mac; assuming minikube and using native kubectl";
+  KUBECTL_CMD="kubectl"
+else
+  echo "$(uname)"
+  echo "Using Linux; assuming microk8s and using microk8s.kubectl";
+  KUBECTL_CMD="microk8s.kubectl"
+fi
+
+# Before you begin you need to have K8s cluster and the kubectl command-line tool congfigured to communicate with your cluster.
 
 # Each node in the cluster must have a least 300 MiB of memory.
 
@@ -7,11 +18,15 @@
 # check if metrics API is available via `kubectl get apiservices` with output containing `metrics.k8s.io`
 
 # Create a namespace so that the resources you create in this exkubectl create namespace mem-example
-microk8s.kubectl create namespace mem-example
+# microk8s.kubectl create namespace mem-example
 
-if [ $(microk8s.kubectl get namespace mem-example | wc -l) -eq 2 ]; then
+SUCCESS=false;
+while ! $SUCCESS; do
+  if [ $($KUBECTL_CMD get namespace mem-example | wc -l) -eq 2 ]; then
     echo "Nice work, you did it!";
-    SUCCESS=True;
-else
+    SUCCESS=true;
+  else
     echo "Failure";
-fi
+    sleep 5s;
+  fi
+done
