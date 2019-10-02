5608d20d8b7a8dff920e2de376aa60c3a938fc2e
mcnichol <mcnichol.m@gmail.com>
2019-10-02 06:27:25 -0500

    Cleanup questions, starting test for detecting pod exists

--- configure-pods-and-containers/01_assign-memory-resources-to-containers-and-pods.sh
+++ configure-pods-and-containers/01_assign-memory-resources-to-containers-and-pods.sh
@@ -1,10 +1,11 @@
 KUBECTL_CMD=""
 
+clear 
+
 if [ "$(uname)" == "Darwin" ]; then
   echo "Using Mac; assuming minikube and using native kubectl";
   KUBECTL_CMD="kubectl"
 else
-  echo "$(uname)"
   echo "Using Linux; assuming microk8s and using microk8s.kubectl";
   KUBECTL_CMD="microk8s.kubectl"
 fi
@@ -17,16 +18,49 @@ fi
 
 # check if metrics API is available via `kubectl get apiservices` with output containing `metrics.k8s.io`
 
-# Create a namespace so that the resources you create in this exkubectl create namespace mem-example
-# microk8s.kubectl create namespace mem-example
+echo "In this exercise, you will create a Pod that has one Container. The Container will have a memory request of 100 MiB and a memory limit of 200 MiB.";
+echo ""
+echo "Create a namespace called mem-example so that resources you create in this Task are isolated"; 
+# Answer: microk8s.kubectl create namespace mem-example
 
 SUCCESS=false;
 while ! $SUCCESS; do
-  if [ $($KUBECTL_CMD get namespace mem-example | wc -l) -eq 2 ]; then
-    echo "Nice work, you did it!";
+  if [ $($KUBECTL_CMD get namespaces --field-selector=metadata.name=mem-example -o custom-columns=NAME:.metadata.name --no-headers | wc -l) -eq 1 ]; then
+    echo "Great job creating the namespace!";
     SUCCESS=true;
   else
-    echo "Failure";
     sleep 5s;
   fi
+done
+
+echo "To specify a memory request and a memory limit. include the 'resources:requests' and 'resources:limits' fields in the Container's resource manifest.";
+echo ""
+echo "Generate the declarative configuration for the Pod with below configuration:"
+echo ""
+echo "Pod Name: memory-demo"
+echo "Image: polinux/stress"
+echo "Container Name: memory-demo-ctr"
+echo "resources.limits.memory: 200Mi"
+echo "resources.requests.memory: 100Mi"
+echo "Container start command: stress"
+echo "Container args array: --vm, 1, --vm-bytes, 150M, --vm-hang, 1"
+
+# microk8s.kubectl get pods -n mem-example -o go-template --template '{{"\n"}}{{range .items}}{{if eq .metadata.name "memory-demo"}}{{.}}{{end}}{{end}}{{"\n\n"}}'
+
+SUCCESS=false;
+while ! $SUCCESS; do
+    if [ "$($KUBECTL_CMD get namespaces)" == "DATA" ]; then
+      SUCCESS=true;
+    else
+      sleep 5s;
+    fi
+done
+
+SUCCESS=false;
+while ! $SUCCESS; do
+    if [ "$($KUBECTL_CMD get namespaces)" == "DATA" ]; then
+      SUCCESS=true;
+    else
+      sleep 5s;
+    fi
 done
